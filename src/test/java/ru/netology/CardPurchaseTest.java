615f8b3b9e981704024f531c53d7c41d6c976ed2
SergiuCon1 <sergiucontevici@gmail.com>
2022-06-15 22:23:04 +0300

    add negative tests

--- src/test/java/ru/netology/CardPurchaseTest.java
+++ src/test/java/ru/netology/CardPurchaseTest.java
@@ -29,7 +29,7 @@ public class CardPurchaseTest {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
     }
 
     @AfterEach
@@ -41,12 +41,136 @@ public class CardPurchaseTest {
     @Test
     void shouldGetSuccessMessage() {
         driver.get("http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Концевич Сергей");
-        elements.get(1).sendKeys("+79245632145");
+        List<WebElement> fieldElements = driver.findElements(By.className("input__control"));
+        fieldElements.get(0).sendKeys("Концевич Сергей");
+        fieldElements.get(1).sendKeys("+79245632145");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
-        String text=driver.findElement(By.className("paragraph")).getText();
+        String text = driver.findElement(By.className("paragraph")).getText();
         assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
+    }
+
+    @Test
+    void shouldGetValidationMessageWhenFioIsEmpty() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.className("button")).click();
+        List<WebElement> fieldElements = driver.findElements(By.className("input__sub"));
+        String text = fieldElements.get(0).getText();
+        assertEquals("Поле обязательно для заполнения", text);
+    }
+
+    @Test
+    void shouldGetValidationMessageWhenPhoneIsEmpty() {
+        driver.get("http://localhost:9999");
+        List<WebElement> fieldElements = driver.findElements(By.className("input__control"));
+        fieldElements.get(0).sendKeys("Концевич Сергей");
+        driver.findElement(By.className("button")).click();
+        List<WebElement> valMessagesElements = driver.findElements(By.className("input__sub"));
+        String text = valMessagesElements.get(1).getText();
+        assertEquals("Поле обязательно для заполнения", text);
+    }
+
+    @Test
+    void shouldGetValidationMessageWhenCheckboxDisabled() {
+        driver.get("http://localhost:9999");
+        List<WebElement> fieldElements = driver.findElements(By.className("input__control"));
+        fieldElements.get(0).sendKeys("Концевич Сергей");
+        fieldElements.get(1).sendKeys("+79245632145");
+        driver.findElement(By.className("button")).click();
+        String color = driver.findElement(By.className("input_invalid")).getCssValue("color");
+        assertEquals("rgba(255, 92, 92, 1)", color);
+    }
+
+    @Test
+    void shouldGetValidationMessageWhenFioWithNumbers() {
+        driver.get("http://localhost:9999");
+        List<WebElement> fieldElements = driver.findElements(By.className("input__control"));
+        fieldElements.get(0).sendKeys("Концевич Сергей123");
+        driver.findElement(By.className("button")).click();
+        List<WebElement> valMessagesElements = driver.findElements(By.className("input__sub"));
+        String text = valMessagesElements.get(0).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text);
+    }
+
+    @Test
+    void shouldGetValidationMessageWhenFioWithSymbols() {
+        driver.get("http://localhost:9999");
+        List<WebElement> fieldElements = driver.findElements(By.className("input__control"));
+        fieldElements.get(0).sendKeys("Концевич Сергей@#@$");
+        driver.findElement(By.className("button")).click();
+        List<WebElement> valMessagesElements = driver.findElements(By.className("input__sub"));
+        String text = valMessagesElements.get(0).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text);
+    }
+
+    @Test
+    void shouldGetValidationMessageWhenFioWithLatin() {
+        driver.get("http://localhost:9999");
+        List<WebElement> fieldElements = driver.findElements(By.className("input__control"));
+        fieldElements.get(0).sendKeys("Contevici Sergiu");
+        driver.findElement(By.className("button")).click();
+        List<WebElement> valMessagesElements = driver.findElements(By.className("input__sub"));
+        String text = valMessagesElements.get(0).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text);
+    }
+
+    @Test
+    void shouldGetValidationMessageWhenPhoneWithoutPlus() {
+        driver.get("http://localhost:9999");
+        List<WebElement> fieldElements = driver.findElements(By.className("input__control"));
+        fieldElements.get(0).sendKeys("Концевич Сергей");
+        fieldElements.get(1).sendKeys("79245632145");
+        driver.findElement(By.className("button")).click();
+        List<WebElement> valMessagesElements = driver.findElements(By.className("input__sub"));
+        String text = valMessagesElements.get(1).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text);
+    }
+
+    @Test
+    void shouldGetValidationMessageWhenPhoneWhenLessThan11() {
+        driver.get("http://localhost:9999");
+        List<WebElement> fieldElements = driver.findElements(By.className("input__control"));
+        fieldElements.get(0).sendKeys("Концевич Сергей");
+        fieldElements.get(1).sendKeys("+7924563214");
+        driver.findElement(By.className("button")).click();
+        List<WebElement> valMessagesElements = driver.findElements(By.className("input__sub"));
+        String text = valMessagesElements.get(1).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text);
+    }
+
+    @Test
+    void shouldGetValidationMessageWhenPhoneWhenMoreThan11() {
+        driver.get("http://localhost:9999");
+        List<WebElement> fieldElements = driver.findElements(By.className("input__control"));
+        fieldElements.get(0).sendKeys("Концевич Сергей");
+        fieldElements.get(1).sendKeys("+792456321456");
+        driver.findElement(By.className("button")).click();
+        List<WebElement> valMessagesElements = driver.findElements(By.className("input__sub"));
+        String text = valMessagesElements.get(1).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text);
+    }
+
+    @Test
+    void shouldGetValidationMessageWhenPhoneWhenAnotherSymbol() {
+        driver.get("http://localhost:9999");
+        List<WebElement> fieldElements = driver.findElements(By.className("input__control"));
+        fieldElements.get(0).sendKeys("Концевич Сергей");
+        fieldElements.get(1).sendKeys("*79245632145");
+        driver.findElement(By.className("button")).click();
+        List<WebElement> valMessagesElements = driver.findElements(By.className("input__sub"));
+        String text = valMessagesElements.get(1).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text);
+    }
+
+    @Test
+    void shouldGetValidationMessageWhenPhoneWhenPlusInTheEnd() {
+        driver.get("http://localhost:9999");
+        List<WebElement> fieldElements = driver.findElements(By.className("input__control"));
+        fieldElements.get(0).sendKeys("Концевич Сергей");
+        fieldElements.get(1).sendKeys("79245632145+");
+        driver.findElement(By.className("button")).click();
+        List<WebElement> valMessagesElements = driver.findElements(By.className("input__sub"));
+        String text = valMessagesElements.get(1).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text);
     }
 }
